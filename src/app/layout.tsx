8e855a1dd874c859b257218702bc1dd7f30c8da2
Gabriel Piva <89921667+gabriel-piva@users.noreply.github.com>
2025-05-28 01:22:33 -0300

    chore: install and setup TanStack Query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from "react";
-import { Space_Grotesk } from "next/font/google";
 import SideBar from "@/app/dashboard/components/SideBar/SideBar";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Space_Grotesk } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
@@ -11,20 +12,30 @@ const spaceGrotesk = Space_Grotesk({
   variable: "--font-space-grotesk",
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 1,
+    },
+  },
+});
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
-
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR" className={spaceGrotesk.variable}>
       <body>
-        <div className="flex bg-[var(--background)] px-4 py-6">
-          <SideBar />
-          <div className="w-full flex-1 px-2 sm:px-4 md:px-6">
-            <main className="h-full w-full overflow-y-auto">{children}</main>
+        <QueryClientProvider client={queryClient}>
+          <div className="flex bg-[var(--background)] px-4 py-6">
+            <SideBar />
+            <div className="w-full flex-1 px-2 sm:px-4 md:px-6">
+              <main className="h-full w-full overflow-y-auto">{children}</main>
+            </div>
           </div>
-        </div>
+        </QueryClientProvider>
       </body>
     </html>
   );
